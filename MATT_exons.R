416b54bf4bb48b57c9cfca9fe4a06771a092613d
estepi <soyestepi@gmail.com>
2021-12-08 13:27:36 +0100

    functions for MATT

---
+++ MATT_exons.R
@@ -0,0 +1,25 @@
+library(MatrixGenerics)
+###########################################
+dPSI_full = read.delim("~/Dropbox (CRG)/LAB_VALCARCEL/Network/tables/dPSI_full_No_Nas.txt",
+                       dec = ",")
+dim(dPSI_full)
+
+dPSI_full_exons = dPSI_full[dPSI_full$COMPLEX %in% c("S", "C1", "C2", "C3", "ANN", "MIC"), ]
+dim(dPSI_full_exons)
+
+dPSI_full_exons[1:5,1:10]
+colnames(dPSI_full_exons)
+summary(dPSI_full_exons$SDcontrols)
+
+
+UP = dPSI_full_exons[dPSI_full_exons$AC008073.5 > 10, "EVENT"]
+#altrernativ no changing
+bg1 = dPSI_full_exons[abs(dPSI_full_exons$AC008073.5) < 5  &
+                        dPSI_full_exons$AVcontrols > 5 &
+                        dPSI_full_exons$AVcontrols < 95,  "EVENT"]
+length(bg1)#500
+#constituve not changing
+bg2 = dPSI_full_exons[abs(dPSI_full_exons$AC008073.5) < 5 
+                      &   dPSI_full_exons$AVcontrols < 5 |
+                        dPSI_full_exons$AVcontrols > 95,  "EVENT"]
+length(bg2)#139
