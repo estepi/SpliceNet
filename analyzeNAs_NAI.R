8b016127480336b63f2319f0fa67abb0cab73efb
estepi <soyestepi@gmail.com>
2022-03-04 21:22:11 +0100

    check some spelling

--- analyzeNAs_NAI.R
+++ analyzeNAs_NAI.R
@@ -1,7 +1,7 @@
 #Estefania March 20222
-#this script count how many NAs each PSI value has.
-#It PSi value has more than 3 NAs it will be replace for NA
-# For IR, if PVal of READ UNBLANCE is signitifcant (<0.5) PIR value will be replaced by NAI
+#This script counts how many NAs each PSI value has.
+#It PSi value has more than 3 NAs it will be replace for NAnew
+# For IR, if pVal of READ UNBLANCE is significant (<0.05) PIR value will be replaced by NAI
 library(data.table)
 library(stringr)
 #########################################
