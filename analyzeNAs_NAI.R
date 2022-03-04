222f97e97763fb94bd5134a9801ab8667b4c9cd1
estepi <soyestepi@gmail.com>
2022-03-04 21:19:57 +0100

    correct pval value

--- analyzeNAs_NAI.R
+++ analyzeNAs_NAI.R
@@ -1,4 +1,7 @@
-#finalMatrixCOmparison
+#Estefania March 20222
+#this script count how many NAs each PSI value has.
+#It PSi value has more than 3 NAs it will be replace for NA
+# For IR, if PVal of READ UNBLANCE is signitifcant (<0.5) PIR value will be replaced by NAI
 library(data.table)
 library(stringr)
 #########################################
