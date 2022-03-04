0e0101bbc8e5422ce687fbaf1e2d6182ecf14980
estepi <soyestepi@gmail.com>
2022-03-04 21:07:07 +0100

    clean analysze NAs_NAIs script

--- analyzeNAs_NAI.R
+++ analyzeNAs_NAI.R
@@ -1,24 +1,17 @@
 #finalMatrixCOmparison
 library(data.table)
 library(stringr)
+#########################################
+#replace new NAs and NAIs at once
+INCFile<-""
+OUTFile1<-""
+OUTFile2<-""
+source("replaceNAI.R")
+replaceNAI(INCFile, OUTFile1,OUTFile2)
+#########################################
+# Only replace new NAs
 INCFile<-""
 OUTFile<-""
 source("replaceNA.R")
 replaceNA(INCFile, OUTFile)
-#########################################
-INCFile<-""
-OUTFile1<-""
-OUTFile2<-""
-source("replaceNAI_corrected.R")
-replaceNAI(INCFile, OUTFile1,OUTFile2)
-#########################################
-infile<-read.table("", 
-                     header=T, sep="\t",na.strings = "NA" , row.names = 1)
-head(infile)
-dim(infile)
-table(infile$COMPLEX)
-nasC<-which(is.na(infile$COMPLEX))
-write.table(infile[nasC,1:6], "", sep="\t", col.names = NA, quote = F)
-rownames(infile)  <-paste(infile$EVENT, 1:nrow(infile), sep="_")
-rownames(infile)[1:10]
 
