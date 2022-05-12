6ba6e208a0547fa493fe91b9695a18c1a63b1217
estepi <soyestepi@gmail.com>
2022-05-12 13:09:57 +0200

    correct

--- prepareA5Table.R
+++ prepareA5Table.R
@@ -3,6 +3,31 @@ library(MatrixGenerics)
 dPSI_full = read.delim("../SpliceNetData//dPSI_full_No_Nas.txt",
                        dec = ",")
 dim(dPSI_full)
+table(dPSI_full$COMPLEX)
+dPSI_full_alt5 = dPSI_full[dPSI_full$COMPLEX %in% "Alt5", ]
+dim(dPSI_full_alt5)
+MIN <- rowMins(as.matrix(dPSI_full_alt5[, 18:322]))
+MAX <- rowMaxs(as.matrix(dPSI_full_alt5[, 18:322]))
+RANGE = MAX - MIN
+Sds <- rowSds(as.matrix(dPSI_full_alt5[, 18:322]))
+dPSI_full_alt5 = cbind(dPSI_full_alt5, RANGE = RANGE)
+dPSI_full_alt5 = cbind(dPSI_full_alt5, Sds = Sds)
+###### remove non changing rows I try different cut offs bigger then 0,5,10,15….
+dPSI_full_chaging = dPSI_full_alt5[dPSI_full_alt5$RANGE > 5, ]
+dim(dPSI_full_chaging) #3676 rows
+table(dPSI_full_chaging$COMPLEX)
+
+colnames(dPSI_full_chaging)[colnames(dPSI_full_chaging) == "LENG1_b"] <-
+  "LENG1"
+colnames(dPSI_full_chaging)[colnames(dPSI_full_chaging) == "RBM17con"] <-
+  "RBM17"
+colnames(dPSI_full_chaging)[colnames(dPSI_full_chaging) == "HFM1_b"] <-
+  "HFM1"
+colnames(dPSI_full_chaging)[colnames(dPSI_full_chaging) == "CCDC12_b"] <-
+  "CCDC12"
+colnames(dPSI_full_chaging)[colnames(dPSI_full_chaging) == "CDC5L_b"] <-
+  "CDC5L"
+dim(dPSI_full_chaging)
 
 class <-
   read.delim(
